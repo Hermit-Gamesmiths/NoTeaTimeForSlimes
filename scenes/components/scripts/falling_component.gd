e9aaa4fabf585855d077f7470dea77aa316995c2
David Dovel <david.dovel@hey.com>
2024-04-13 16:16:01 -0500

    Hack together blocks that fall on contact

---
+++ scenes/components/scripts/falling_component.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@export var is_active: bool = false
+
+@export var falling_speed = 400
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	var parent: PhysicsBody2D = get_parent()
+	if is_active:
+		parent.move_and_collide(Vector2(0, falling_speed) * delta)
+	pass
+
+func activate() -> void:
+	is_active = true
---
+++ scenes/props/falling_platform.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	move_and_collide(Vector2(100, 0) * delta)
+	pass
