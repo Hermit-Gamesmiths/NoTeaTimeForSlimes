f7cc2a616c7bf0d0a488ccab13f183a8de216a58
David Dovel <david.dovel@hey.com>
2024-04-15 16:31:48 -0500

    Kill the wizard in stage 1

--- global/game.gd
+++ global/game.gd
@@ -2,32 +2,45 @@ extends Node
 
 @export var level_list:LevelCollection
 @export_file("*.tscn", "*.scn") var end_scene := ""
+@export_file("*.tscn", "*.scn") var good_end_scene := ""
 
 
 var level_index: int = 0
 var current_level: Level
 var current_level_scene: String
+var has_killed_wizard:bool = false
+
+var times_wizard_died:int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _enter_tree() -> void:
 	Events.level_loaded.connect(level_loaded)
 	Events.player_died.connect(player_died)
 	Events.level_finished.connect(level_complete)
+	Events.wizard_died.connect(wizard_died)
 
 
 func level_loaded(level: Level) -> void:
-	print("level loaded")
+	has_killed_wizard = false
 	current_level_scene = level.scene_file_path
 	current_level = level
 
 
 func player_died() -> void:
+	has_killed_wizard = false
 	TransitionManager.transition_to(current_level_scene)
 	pass
 
 
+func wizard_died() -> void:
+	has_killed_wizard = true
+
+
 func level_complete() -> void:
 	level_index = level_index + 1
+	if has_killed_wizard:
+		times_wizard_died += 1
+
 	if level_index >= len(level_list.levels):
 		level_index = 0
 		print("Fin")
