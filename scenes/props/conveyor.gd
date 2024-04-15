8c502ed56e5d18b5a6bbeda543cc0170b1d80611
David Dovel <david.dovel@hey.com>
2024-04-15 11:51:39 -0500

    Add switchable conveyors

---
+++ scenes/props/conveyor.gd
@@ -0,0 +1,45 @@
+@tool
+extends StaticBody2D
+
+enum Mode {
+	Flip,
+	Enable,
+}
+
+enum Direction {
+	Left = -1,
+	Right = 1,
+}
+
+@export var is_active: bool = true
+@export var activator_mode: Mode = Mode.Enable
+@export var speed: float = 300
+@export var direction: Direction = Direction.Left
+@onready var current_direction: int = direction
+
+func activate():
+	print("toggle conveyor")
+	toggle(true)
+
+
+func deactivate():
+	toggle(false)
+
+
+func toggle(switch:bool):
+	match activator_mode:
+		Mode.Flip:
+			current_direction = direction * -1 if switch else 1
+		Mode.Enable:
+			is_active = switch
+
+func _process(_delta: float)->void:
+	scale.x = -current_direction
+	constant_linear_velocity = Vector2(speed * current_direction, 0) if is_active else Vector2.ZERO
+
+	if not Engine.is_editor_hint():
+		if is_active:
+			$AnimationPlayer.play("active")
+		else:
+			print("inactive")
+			$AnimationPlayer.play("inactive")
