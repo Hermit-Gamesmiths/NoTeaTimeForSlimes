806317aec2d09fe72fe683553c18af4c838c521f
David Dovel <david.dovel@hey.com>
2024-04-14 21:53:38 -0500

    Remove some printing

--- scenes/components/scripts/edible.gd
+++ scenes/components/scripts/edible.gd
@@ -14,7 +14,6 @@ func _ready() -> void:
 func pack(target:Node2D) -> void:
 	# TODO: send true and animate flowing into the player/shrinking using a tween.
 	parent_rotation = parent.global_rotation
-	print("ate rotation ", parent_rotation)
 	self.reparent(target, true)
 	parent.get_parent().remove_child(parent)
 	global_rotation = 0
@@ -24,7 +23,6 @@ func unpack(world: Node, location: Vector2) -> void:
 	# HACK: This is a workaround to an INSANE issue with the previous position of the object
 	var colliders = parent.find_children("*", "CollisionShape2D", true, false)
 	for child:CollisionShape2D in colliders:
-		print("OFF")
 		child.disabled = true
 	parent.global_position = location
 	world.add_child(parent)
